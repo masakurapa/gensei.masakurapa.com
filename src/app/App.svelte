68d81ad7f688fa336bd521fbd11e24fa389aa574
Masahiro Sakuraba <rabasan1031@gmail.com>
2019-07-26 22:59:18 +0900

    globalなcssを移動してみる

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -37,3 +37,70 @@
         component = components[event.target.value].component
     }
 </script>
+
+<style>
+    :global(html, body) {
+        position: relative;
+        width: 100%;
+        height: 100%;
+    }
+
+    :global(body) {
+        background: #F5FFFA !important;
+        color: #2c3e50 !important;
+        font-family: 'Hannotate SC','Chalkboard SE','ＭＳ 明朝', 'MS Mincho',sans-serif !important;
+        margin: 0;
+    }
+
+    :global(h1) {
+        font-size: 40px !important;
+        font-weight: bold !important;
+        text-align: center !important;
+        margin-block-start: 0 !important;
+        margin-block-end: 0 !important;
+        margin-inline-start: 0 !important;
+        margin-inline-end: 0 !important;
+    }
+
+    :global(h2) {
+        font-size: 25px !important;
+        font-weight: bold !important;
+        text-align: center !important;
+        margin-block-start: 0 !important;
+        margin-block-end: 0 !important;
+        margin-inline-start: 0 !important;
+        margin-inline-end: 0 !important;
+    }
+
+    :global(.bold-text) {
+        font-weight: bold;
+        color: #be2525;
+    }
+
+    :global(.main-wrapper) {
+        min-height: 100vh;
+        position: relative;
+        padding-bottom: 100px;
+        box-sizing: border-box;
+    }
+
+    :global(.footer) {
+        width: 100%;
+        background-color: #5db1cf;
+        color: #fff;
+        text-align: center;
+        padding: 15px 0;
+        position: absolute;
+        bottom: 0;
+    }
+
+    :global(.copyright) {
+        margin-bottom: 5px;
+    }
+
+    :global(.footer-link i) {
+        position: relative;
+        top: 5px;
+        right: 5px;
+    }
+</style>
