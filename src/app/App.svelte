e98b68202db82a99592bbe1f660fc219980f415c
Masahiro Sakuraba <rabasan1031@gmail.com>
2019-07-26 23:04:56 +0900

    Componentを分離

--- src/app/App.svelte
+++ src/app/App.svelte
@@ -4,38 +4,12 @@
 </CollapseSimpleFrame>
 
 <UserList/>
-
-<CollapseFrame id="components" label="②抽選方法を選ぶ">
-    <InputSelect
-        options="{components}"
-        on:change="{changeComponent}"
-    ></InputSelect>
-</CollapseFrame>
-
-<svelte:component this={component}/>
+<Component/>
 
 <script>
-    import InputSelect from 'parts/input/InputSelect.svelte'
-
-    import UserList from 'components/common/userList/Index.svelte'
-    import CollapseFrame from 'components/common/collapse/CollapseFrame.svelte'
+    import Component from 'components/Component.svelte'
     import CollapseSimpleFrame from 'components/common/collapse/CollapseSimpleFrame.svelte'
-
-    import RandomSelect from 'components/randomSelect/RandomSelect.svelte'
-    import Amidakuji from 'components/amidakuji/Amidakuji.svelte'
-    import Slot from 'components/slot/Slot.svelte'
-
-    const components = [
-        { value: 0, text: '', component: null },
-        { value: 1, text: 'ランダムで選ぶ', component: RandomSelect },
-        { value: 2, text: 'あみだくじで選ぶ', component: Amidakuji },
-        { value: 3, text: 'スロットで選ぶ（β版）', component: Slot },
-    ]
-
-    let component = null
-    function changeComponent (event) {
-        component = components[event.target.value].component
-    }
+    import UserList from 'components/common/userList/Index.svelte'
 </script>
 
 <style>
---
+++ src/app/components/Component.svelte
@@ -0,0 +1,29 @@
+<CollapseFrame id="components" label="②抽選方法を選ぶ">
+    <InputSelect
+        options="{components}"
+        on:change="{changeComponent}"
+    ></InputSelect>
+</CollapseFrame>
+
+<svelte:component this={component}/>
+
+<script>
+    import InputSelect from 'parts/input/InputSelect.svelte'
+    import CollapseFrame from 'components/common/collapse/CollapseFrame.svelte'
+
+    import RandomSelect from 'components/randomSelect/RandomSelect.svelte'
+    import Amidakuji from 'components/amidakuji/Amidakuji.svelte'
+    import Slot from 'components/slot/Slot.svelte'
+
+    const components = [
+        { value: 0, text: '', component: null },
+        { value: 1, text: 'ランダムで選ぶ', component: RandomSelect },
+        { value: 2, text: 'あみだくじで選ぶ', component: Amidakuji },
+        { value: 3, text: 'スロットで選ぶ（β版）', component: Slot },
+    ]
+
+    let component = null
+    function changeComponent (event) {
+        component = components[event.target.value].component
+    }
+</script>
