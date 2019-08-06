053e7e6e8c8bd04ed91bf68d866a28b8b0e5a928
Masahiro Sakuraba <rabasan1031@gmail.com>
2019-08-06 21:01:57 +0900

    App.svelteのダイエット

---
+++ src/app/components/Layout.svelte
@@ -0,0 +1,14 @@
+<CollapseSimpleFrame id="header-description" label="ツールについて">
+    <div>ちょっとしたこと、なかなか決められないことなどなど...ツールでサクッと決めちゃいましょう！</div>
+    <div>ランダムで選ぶだけではなく、あみだくじ、スロット形式で気分を変えながら抽選ができます！</div>
+</CollapseSimpleFrame>
+
+<UserList/>
+
+<Component/>
+
+<script>
+    import Component from 'components/Component.svelte'
+    import CollapseSimpleFrame from 'components/common/collapse/CollapseSimpleFrame.svelte'
+    import UserList from 'components/common/userList/Index.svelte'
+</script>
