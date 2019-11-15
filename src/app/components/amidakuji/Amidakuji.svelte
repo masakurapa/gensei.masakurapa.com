09f37eb90a19c9ef8efdb0252ddfb5f026daa6ad
Masahiro Sakuraba <rabasan1031@gmail.com>
2019-11-15 23:48:26 +0900

    抽選方法選択時に数字がリセットされない

--- src/app/components/amidakuji/Amidakuji.svelte
+++ src/app/components/amidakuji/Amidakuji.svelte
@@ -5,10 +5,14 @@
 <script>
     import { onMount } from 'svelte'
     import { showUserList } from 'app/store.js'
+    import { selectedNumber } from 'components/amidakuji/store.js'
 
     import Input from 'components/amidakuji/Input.svelte'
     import Button from 'components/amidakuji/Button.svelte'
     import Result from 'components/amidakuji/Result.svelte'
 
-    onMount(() => showUserList.set(true))
+    onMount(() => {
+        showUserList.set(true)
+        selectedNumber.set([])
+})
 </script>
