1f3abfbe4caf13b30ae204bd8d635831cc9c5629
masakurapa <rabasan1031@gmail.com>
2022-05-01 22:34:21 +0900

    randomSelectのscriptをまとめる

--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,7 @@
 /**
  * 0 ~ n までのランダムな数字を返す
+ * @param  {number} n
+ * @returns number
  */
 export const random = (n: number): number => {
     return Math.floor(Math.random() * n);
@@ -7,6 +9,8 @@ export const random = (n: number): number => {
 
 /**
  * 0 ~ n までのランダムな偶数を返す
+ * @param  {number} n
+ * @returns number
  */
 export const randomEven = (n: number): number => {
     return random(n / 2) * 2;
@@ -14,6 +18,8 @@ export const randomEven = (n: number): number => {
 
 /**
  * 0 ~ n までのランダムな奇数を返す
+ * @param  {number} n
+ * @returns number
  */
 export const randomOdd = (n: number): number => {
     return random(n / 2) * 2 + 1;
@@ -21,13 +27,26 @@ export const randomOdd = (n: number): number => {
 
 /**
  * スリープ用のPromiseを返す
+ * @param  {number} ms
+ * @returns Promise
  */
 export const sleep = (ms: number): Promise<void> => {
     return new Promise(resolve => setTimeout(() => resolve(), ms));
 };
 
 /**
+ * string -> int
+ * @param  {string} val
+ * @returns number
+ */
+export const toInt = (val: string): number => {
+    return parseInt(val, 10);
+}
+
+/**
  * 配列をシャッフルする
+ * @param  {T[]} target
+ * @returns T
  */
 export const shuffle = <T> (target: T[]): T[] => {
     const result = target.concat();
@@ -42,6 +61,7 @@ export const shuffle = <T> (target: T[]): T[] => {
 
 /**
  * UAがスマートフォンか判定する
+ * @returns boolean
  */
 export const isSmartPhone = (): boolean => {
     const ua = navigator.userAgent;
