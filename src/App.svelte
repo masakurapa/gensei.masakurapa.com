7035fed5e89d4d83b2cc2eb1815cfa2b4949dba7
masakurapa <rabasan1031@gmail.com>
2022-06-12 00:27:49 +0900

    一時的にv2.0.0を落とす

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,13 @@
 {#if isSmartPhone()}
     <LegacyLayout/>
 {:else}
-    <Layout/>
+    <LegacyLayout/>
+    <!-- <Layout/> -->
 {/if}
 
 <script lang="ts">
     import { isSmartPhone } from './util';
 
-    import Layout from './Layout.svelte';
+    // import Layout from './Layout.svelte';
     import LegacyLayout from './legacy/components/Layout.svelte';
 </script>
