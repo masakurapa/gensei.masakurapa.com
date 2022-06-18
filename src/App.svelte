c862afecc80576bc2b3a5488924642a5e1d60f00
masakurapa <rabasan1031@gmail.com>
2022-06-18 23:55:04 +0900

    App.svelteでlegacyを読み込まない

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,5 @@
-{#if isSmartPhone()}
-    <LegacyLayout/>
-{:else}
-    <Layout/>
-{/if}
+<Layout/>
 
 <script lang="ts">
-    import { isSmartPhone } from './util';
-
     import Layout from './Layout.svelte';
-    import LegacyLayout from './legacy/components/Layout.svelte';
 </script>
